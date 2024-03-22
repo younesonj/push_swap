950ca5a036a646d984821d3ea294c76617798712
Younes Ounajjar <younajja@e1r10p12.1337.ma>
2024-03-22 16:47:27 +0000

    starting

---
+++ push_swap.c
@@ -0,0 +1,24 @@
+#include "push_swap.h"
+
+char    **read_numbers(char **arr_nb, int size)
+{
+    char **res = (char **)malloc(size * sizeof(char *));
+    int i = 1;
+    int j = 0;
+    while (i < size)
+    {
+        
+    }
+    
+}
+
+
+int main (int ac, char **av)
+{
+    if (ac < 2)
+    {
+        printf("error");
+        exit(1);
+    }
+    
+}
---
+++ push_swap.h
@@ -0,0 +1,16 @@
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+char	**ft_split(char const *s, char c);
+size_t	ft_strlen(const char *str);
+static char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strdup(const char *src);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+
+#endif
