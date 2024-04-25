55e5236fc918414f717d81ba35c71c3d9f1d5f7e
Younes Ounajjar <younajja@e1r10p4.1337.ma>
2024-04-25 19:06:38 +0100

    p

--- sorting/stack_sort.c
+++ sorting/stack_sort.c
@@ -1,11 +1,21 @@
 #include "../push_swap.h"
 
+t_list  *search_smallest_cost(t_list **list)
+{
+    Node *curr = *list;
+    Node *min = *list;
+    while (curr != NULL)
+    {
+        if (min->cost_to_push > curr->cost_to_push)
+            min = curr;
+        curr = curr->next;
+    }
+    return (min);
+} 
 
 
-
-
-
-
+void	push(t_list **stack_a, t_list **stack_b)
+{}
 
 void    stack_sort(t_list **stack_a, t_list **stack_b)
 {
@@ -17,7 +27,7 @@ void    stack_sort(t_list **stack_a, t_list **stack_b)
         set_positions(stack_b);
         set_target(stack_a, stack_b);
         set_cost_to_push(*stack_a, len_list(stack_a), len_list(stack_b));
-
+		push(stack_a, stack_b);
         
     }
 }
